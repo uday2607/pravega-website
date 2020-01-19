8cedcefd351bb00da2ae98d8bd857e0793c24b4f
Uday-2607 <50131228+Uday-2607@users.noreply.github.com>
2020-01-19 11:53:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.use(cors());
 app.use(express.static(__dirname));
 app.use(sslRedirect());
 
+app.use(forceDomain({
+      hostname: 'www.pravega.com',
+      protocol: 'https'
+    }));
 
 
 if(process.env.NODE_ENV === 'production') {
@@ -44,10 +48,57 @@ app.use(function(req, res, next) {
         next();
     });
 
-app.use(forceDomain({
-      hostname: 'www.pravega.com',
-      protocol: 'https'
-    }));
+    var express = require('express'),
+        routes = require('./routes'),
+        upload = require('./routes/upload'),
+        http = require('http'),
+        https = require('https'),
+        fs = require('fs'),
+        path = require('path'),
+        httpApp = express(),
+        app = express(),
+        certPath = "cert";
+
+
+    var httpsOptions = {
+        key: fs.readFileSync(path.join(certPath, "ssl.key")),
+        cert: fs.readFileSync(path.join(certPath, "ssl.crt"))
+    };
+    httpApp.set('port', process.env.PORT || 80);
+    httpApp.get("*", function (req, res, next) {
+        res.redirect("https://" + req.headers.host + "/" + req.path);
+    });
+
+    // all environments
+    app.set('port', process.env.PORT || 443);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.enable('trust proxy');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    // development only
+    if('development' == app.get('env')) {
+        app.use(express.errorHandler());
+    }
+
+    app.get('/', routes.index);
+    app.post('/upload', upload.s3);
+
+
+    http.createServer(httpApp).listen(httpApp.get('port'), function() {
+        console.log('Express HTTP server listening on port ' + httpApp.get('port'));
+    });
+
+    https.createServer(httpsOptions, app).listen(app.get('port'), function() {
+        console.log('Express HTTPS server listening on port ' + app.get('port'));
+    });     
+
+
 
 
 // Base route
